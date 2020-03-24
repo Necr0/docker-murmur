f2cd7968e45fc589d1788bf59b8614fb7cd9f6c9
Necro <andre.kramp@necro-development.net>
2020-03-24 16:53:06 +0100

    Initial version

---
+++ Dockerfile
@@ -0,0 +1,49 @@
+FROM alpine:latest
+MAINTAINER Necro <necro@necro.dev>
+
+ARG MURMUR_DATA=/var/lib/murmur
+
+RUN apk add --no-cache murmur bash && \
+    mkdir -p /etc/murmur
+
+COPY ./run.sh /bin/run-murmur
+COPY ./murmur.ini /etc/murmur.ini
+
+ENV MURMUR_DATABASE="$MURMUR_DATA/murmur.sqlite" \
+    MURMUR_DBDRIVER="QSQLITE" \
+    MURMUR_ICE="tcp -h 127.0.0.1 -p 6502" \
+    MURMUR_AUTOBANATTEMPTS="10" \
+    MURMUR_AUTOBANTIMEFRAME="120" \
+    MURMUR_AUTOBANTIME="300" \
+    MURMUR_WELCOMETEXT="<br />Welcome to this server running <b>Murmur</b>.<br />Enjoy your stay!<br />" \
+    MURMUR_PORT="64738" \
+    MURMUR_BANDWIDTH="128000" \
+    MURMUR_USERS="100" \
+    MURMUR_MESSAGEBURST="5" \
+    MURMUR_MESSAGELIMIT="1" \
+    MURMUR_ALLOWPING="true" \
+    MURMUR_OPUSTHRESHHOLD="100" \
+    MURMUR_CHANNELNESTINGLIMIT="10" \
+    MURMUR_CHANNELCOUNTLIMIT="1000" \
+    MURMUR_CHANNELNAME="[ \\-=\\w\\#\\[\\]\\{\\}\\(\\)\\@\\|]+" \
+    MURMUR_USERNAME="[-=\\w\\[\\]\\{\\}\\(\\)\\@\\|\\.]+" \
+    MURMUR_TEXTMESSAGELENGTH="5000" \
+    MURMUR_IMAGEMESSAGELENGTH="131072" \
+    MURMUR_ALLOWHTML="true" \
+    MURMUR_LOGDAYS="-1" \
+    MURMUR_BONJOUR="False" \
+    MURMUR_SSLDHPARAMS="@ffdhe2048" \
+    MURMUR_SSLCIPHERS="EECDH+AESGCM:EDH+aRSA+AESGCM:DHE-RSA-AES256-SHA:DHE-RSA-AES128-SHA:AES256-SHA:AES128-SHA" \
+    MURMUR_UNAME="murmur" \
+    MURMUR_CERTREQUIRED="False" \
+    MURMUR_SENDVERSION="True" \
+    MURMUR_LEGACYPASSWORDHASH="false" \
+    MURMUR_KDFITERATIONS="-1" \
+    MURMUR_ICE_WARN_UNKNOWNPROPERTIES="1" \
+    MURMUR_ICE_MESSAGESIZEMAX="65536"
+    
+
+EXPOSE 64738/tcp 64738/udp
+VOLUME $MURMUR_DATA
+
+CMD ["/bin/bash", "/bin/run-murmur"]
---
+++ run.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Use /etc/murmur.ini as a template and interpolate variables for the environment and write them to /etc/murmur/config.ini.
+# This has to be done in order to configure murmur based on environment variables because it does not support setting config option through parameters or environment variables, which is quite stupid IMO
+# If /etc/murmur/config.ini already exists this step is skipped.
+function interpolate_vars {
+    # This function is taken from: https://stackoverflow.com/a/2916159
+    line="$(cat; echo -n a)"
+    end_offset=${#line}
+    while [[ "${line:0:$end_offset}" =~ (.*)(\$\{([a-zA-Z_][a-zA-Z_0-9]*)\})(.*) ]] ; do
+        PRE="${BASH_REMATCH[1]}"
+        POST="${BASH_REMATCH[4]}${line:$end_offset:${#line}}"
+        VARNAME="${BASH_REMATCH[3]}"
+        eval 'VARVAL="$'$VARNAME'"'
+        line="$PRE$VARVAL$POST"
+        end_offset=${#PRE}
+    done
+    echo -n "${line:0:-1}"
+}
+if [ ! -f /etc/murmur/config.ini ]; then
+    interpolate_vars < /etc/murmur.ini > /etc/murmur/config.ini;
+else
+    echo "Static config file found, skipping templating."
+fi;
+
+
+if [ "$MURMUR_DEBUG_LOG_CONFIG" == "true" ]; then
+    echo "######## BEGINNING OF MURMUR CONFIG ############";
+    cat /etc/murmur/config.ini;
+    echo "######## END OF MURMUR CONFIG ##################";
+fi;
+
+
+# Set the SuperUser password the environment variable is set
+# To do this we have to start murmur with the -supw parameter which makes it set the password and exit, which is quite stupid IMO
+if [ ! -z "$MURMUR_SUPERUSER_PASSWORD" ]; then
+    murmurd -fg -v -ini /etc/murmur/config.ini -supw "$MURMUR_SUPERUSER_PASSWORD"
+fi;
+
+# Start murmur in foreground mode
+murmurd -fg -v -ini /etc/murmur/config.ini
